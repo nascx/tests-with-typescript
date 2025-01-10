d8d31fd5a81a115a9f252e97e3385fc01f7ce85a
Wesley Silva do Nascimento <wnascimento@MKMONDIAL.local>
2025-01-10 16:21:30 -0400

    docs - create readme

--- api.test.ts
+++ api.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from './api';
 
 test('GET /', async () => {
-    const response = await request(app).get('/')
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual({message: 'Hi'})
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({message: 'Hi'});
 })
---
+++ readme.md
@@ -0,0 +1,9 @@
+### Tests with typescript 
+
+This is repositorie is based in tutorial below:
+
+A
+B
+
+Before write code and test lets understand about two tests types: Unitarie tests and integration tests
+
